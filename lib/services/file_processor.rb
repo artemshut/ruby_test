400d25d472698844cdb6b90ba4c937fc34055f6a
Artem Shut <artem.shut@aterise.com>
2021-06-13 20:59:57 +0200

    Add initial file processing

---
+++ lib/services/file_processor.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Services
+  ##
+  # This class represents initial file processing i.e. validation
+  # and log entries structure
+  #
+  class FileProcessor
+    attr_reader :file_path
+
+    def initialize(file_path)
+      @file_path = file_path
+      @log_entries = []
+    end
+
+    def process_file
+      validate_file
+      store_log_entries
+    end
+
+    private
+
+    def validate_file
+      Validators::FileValidator.new(file_path).validate
+    end
+
+    def store_log_entries
+      File.foreach(file_path) do |line|
+        log_entry_url, log_entry_ip_address = line.split(' ')
+        @log_entries << Entities::LogEntry.new(log_entry_url, log_entry_ip_address)
+      end
+      @log_entries
+    end
+  end
+end
