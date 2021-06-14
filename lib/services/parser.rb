aa2318b94f251ce4e5228b6a9d0a112afc54d367
Artem Shut <artem.shut@aterise.com>
2021-06-14 12:07:17 +0200

    Add main parser file to process logs

---
+++ lib/services/parser.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Services
+  ##
+  # This class represents the main entry of the whole parsing process
+  #
+  class Parser
+    attr_reader :file_path
+
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def parse_file
+      process_log_file
+      collect_statistics
+      display_formatted_statistics
+    end
+
+    private
+
+    attr_reader :log_entries, :log_statistics
+
+    def process_log_file
+      @log_entries = FileProcessor.new(file_path).process_file
+    end
+
+    def collect_statistics
+      @log_statistics = StatisticsCollector.new(log_entries).collect
+    end
+
+    def display_formatted_statistics
+      StatisticsFormatter.new(log_statistics).display_formatted_statistics
+    end
+  end
+end
